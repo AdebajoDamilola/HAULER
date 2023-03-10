84fead544df97b62234c8aea66f32b2de4afa68d
Promise Orung <magnificentorung@gmail.com>
2023-03-10 05:29:54 +0100

    Implement design for footer

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Benefits from "../components/Benefits.js";
+import Footer from "../components/Footer.js";
 import Navbar from "../components/Navbar.js";
 import Rental from "../components/Rental.js";
+
 
 function Home() {
   return (
     <>
       <Navbar />
       <Rental />
+      <Footer />
     </>
   );
 }
